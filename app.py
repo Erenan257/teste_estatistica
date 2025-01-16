20787f67008b8d3007de5af698b25b8584e098dc
renan <renan@renan.com>
2025-01-16 15:38:47 -0300

    comit 1

--- app.py
+++ app.py
@@ -16,5 +16,6 @@ def index():
     return render_template('index.html', usuarios=usuarios)
 
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
