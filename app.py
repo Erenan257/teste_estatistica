8c122851ac702c76f60efc9b8490cf5790b2c1bf
Renan <erenan257@gmail.com>
2025-01-24 01:05:32 -0300

    git em casa

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+# filepath: /home/renan/Documentos/teste_estatistica/teste_estatistica/app.py
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meu_banco.db'
@@ -9,6 +11,10 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     senha = db.Column(db.String(120), nullable=False)
+
+# Cria todas as tabelas
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
@@ -23,14 +29,3 @@ def adicionar():
     # Verifica se o email já está registrado
     if Usuario.query.filter_by(email=email).first():
         return "Email já registrado!"
-
-    novo_usuario = Usuario(email=email, senha=senha)
-    db.session.add(novo_usuario)
-    db.session.commit()
-
-    return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
